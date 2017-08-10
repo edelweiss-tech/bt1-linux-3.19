f05f08c3d87cad38b07df50b63b0665e2cfdac35
Konstantin Kirik (snegovick) <snegovick@uprojects.org>
2017-08-11 00:46:42 +0300

    Fixed "too much work for irq55"

--- drivers/tty/serial/8250/8250_core.c
+++ drivers/tty/serial/8250/8250_core.c
@@ -1610,6 +1610,12 @@ int serial8250_handle_irq(struct uart_port *port, unsigned int iir)
 
 		if (!up->dma || dma_err)
 			status = serial8250_rx_chars(up, status);
+	} else if ((iir & 0x3f) == UART_IIR_RX_TIMEOUT) {
+		/*
+		 * On some systems we saw the timeout interrupt even when
+		 * there was no data ready.  Do a bogus read to clear it.
+		 */
+		(void) serial_port_in(port, UART_RX);
 	}
 	serial8250_modem_status(up);
 	if ((!up->dma || (up->dma && up->dma->tx_err)) &&
